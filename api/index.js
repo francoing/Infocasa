dc5498359ff5a2699aed6fadc5d8e72d29582754
Franco.Montti <38160834+francoing@users.noreply.github.com>
2026-05-18 09:18:35 -0300

    Fix: Move db.json to api folder to fix module resolution in Vercel

--- api/index.js
+++ api/index.js
@@ -2,8 +2,8 @@ import jsonServer from 'json-server';
 import { createRequire } from 'module';
 
 const require = createRequire(import.meta.url);
-const db = require('../db.json');
-const routes = require('../routes.json');
+const db = require('./db.json');
+const routes = require('./routes.json');
 
 const server = jsonServer.create();
 const router = jsonServer.router(db);
